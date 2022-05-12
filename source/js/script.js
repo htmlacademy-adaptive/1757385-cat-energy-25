ad13d5917b56ca286ca04d9ea191aae89988403b
Gentlecat-star <81353321+Gentlecat-star@users.noreply.github.com>
2022-05-12 20:22:33 +0600

    Add webp optimization in css. Fix some troubles

--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,14 @@ let navMain = document.querySelector('.main-nav');
 let headerMain = document.querySelector('.main-header');
 let body = document.querySelector('.page__body');
 
+var isSupportWebp =
+  !![].map && document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') === 0
+if (isSupportWebp) {
+  body.classList.add('webp');
+} else {
+  body.classList.add('no-webp');
+}
+
 headerMain.classList.remove('no-js');
 
 navButton.addEventListener('click', function () {
